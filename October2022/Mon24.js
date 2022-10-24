2c72b7c618ce364dcf1457aa2678fe6ddb1a5900
DB-Butter <devbabs68@gmail.com>
2022-10-24 14:05:50 -0500

    Mon24 outline

--- October2022/Mon24.js
+++ October2022/Mon24.js
@@ -4,3 +4,13 @@
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 // You can return the answer in any order.
+
+// myAnswer:
+
+// create a function which accepts an array of numbers, and a target number
+
+// create a for loop which iterates over the array
+
+// create a nested for loop which checks if [i] + [j] = target number
+
+//return result
