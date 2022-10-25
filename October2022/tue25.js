e2a832c3943c89dfc23f04c9315c17d57e27caa5
DB-Butter <devbabs68@gmail.com>
2022-10-25 12:44:10 -0500

    init Tue25

---
+++ October2022/tue25.js
@@ -0,0 +1,5 @@
+// Tuesday October 25, 2022. Challenge: easy.
+
+// Challenge:
+// Given an integer x, return true if x is palindrome integer.
+// An integer is a palindrome when it reads the same backward as forward.
