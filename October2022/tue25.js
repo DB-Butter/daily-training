3264ffe121eb7b167d2b6433ff88272d2ad33b2d
DB-Butter <devbabs68@gmail.com>
2022-10-25 12:49:06 -0500

    Tue25 outline

--- October2022/tue25.js
+++ October2022/tue25.js
@@ -3,3 +3,13 @@
 // Challenge:
 // Given an integer x, return true if x is palindrome integer.
 // An integer is a palindrome when it reads the same backward as forward.
+
+// myAnswer:
+
+// turn the number into an array
+
+// create a new array which consists of the original array backwards
+
+// compare the arrays
+
+// return result
