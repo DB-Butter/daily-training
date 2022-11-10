621b08f276b485cd194b06cfd83111bf7f5240d1
DB-Butter <devbabs68@gmail.com>
2022-11-10 09:56:33 -0600

    challenge nov/thu10

--- November2022/thu10.js
+++ November2022/thu10.js
@@ -1,2 +1,9 @@
 // Thursday November10, 2022. Challenge: easy.
 
+//Challenge:
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+
