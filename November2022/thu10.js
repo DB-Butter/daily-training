b9ee15fbfab417e27848159b5ac802ef246a0847
DB-Butter <devbabs68@gmail.com>
2022-11-10 09:56:46 -0600

    answer nov/thu10

--- November2022/thu10.js
+++ November2022/thu10.js
@@ -7,3 +7,10 @@
 // If there is no common prefix, return an empty string "".
 
 
+// myAnswer: 
+
+// create an answer array
+
+// compare string1[i] by stringX[i] if 1[i] === X[i], then push 1[i] into the answer array
+
+// join the answer array for result string (if empty return '')
