98762aba31e2658e49774b28e9b997d9bde95edb
DB-Butter <devbabs68@gmail.com>
2022-11-10 10:39:54 -0600

    attempt 1

--- November2022/thu10.js
+++ November2022/thu10.js
@@ -11,8 +11,39 @@
 
 // find the string with the lowest length
 
-// create result string
-
-// collect arr[0][j] as current character; compare current character across arr[i][j] and append result string 
+// collect current character; compare current character across arr[i][j] and append result string
 
 // return result
+
+// dictate array and array.length and run
+
+function shortestString(arr,arrLength) {
+    let min = arr[0].length;
+    for (let i=1;i<arrLength;i++) {
+        if (arr[i].length < min) {
+            min = arr[i].length;
+        }
+    }
+    return (min);
+}
+
+function findLongestPrefix(arr,arrLength) {
+    let shortString = shortestString(arr, arrLength)
+    let result = ""
+    let currentChar
+    for (let i=0;i<shortString;i++) {
+        currentChar = arr[0][i]
+   
+        for (let j=1;j<arrLength;j++) {
+            if (arr[j][i] != currentChar) {
+                return result
+            }
+        }   
+        result += (currentChar)
+    }
+    return (result)
+}
+
+let arr = ['test', 'testing', 'tester']
+let arrLength = arr.length
+console.log(findLongestPrefix(arr, arrLength))
