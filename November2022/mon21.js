a91b6c76b2b8d131c20cbca2459e651612edfcfe
DB-Butter <devbabs68@gmail.com>
2022-11-21 13:27:03 -0600

    attempt1 mon21/November2022

--- November2022/mon21.js
+++ November2022/mon21.js
@@ -14,3 +14,10 @@
 
 // Sort result list from least to greatest
 
+function mergeAndSort (arr1, arr2) {
+    let arr3 = arr1.concat(arr2)
+    arr3.sort(function(a, b) {
+        return a - b;
+    });
+    console.log(arr3)
+}
