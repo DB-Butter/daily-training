1906ca93153d0a0ab4614cf7ce535b9494ce1b96
DB-Butter <devbabs68@gmail.com>
2022-11-22 13:35:52 -0600

    example2 November2022/tue22

--- November2022/tue22.js
+++ November2022/tue22.js
@@ -23,6 +23,9 @@ function removeDupes (arr) {
         if (arr[i] === arr[i-1]) {
             arr.splice(i, 1)
         }
+        if (arr[i] === arr[i+1]) {
+            arr.splice(i, 1)
+        }
     }
     console.log(`${arr.length}, nums = [${arr}]`)
 }
@@ -34,4 +37,13 @@ removeDupes([1,1,2])
 // Input: nums = [1,1,2]
 // Output: 2, nums = [1,2,_]
 // Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+// Example 2:
+
+removeDupes([0,0,1,1,1,2,2,3,3,4])
+
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
