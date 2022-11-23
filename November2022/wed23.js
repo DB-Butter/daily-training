cfd2957efe228e5f512e3b8dbcd14e8e2c7fec09
DB-Butter <devbabs68@gmail.com>
2022-11-23 14:49:20 -0600

    example1 November2022/wed23

--- November2022/wed23.js
+++ November2022/wed23.js
@@ -26,3 +26,14 @@ function removeElement(arr, val) {
     }
     console.log(`${arr.length}, nums = [${arr}]`)
 }
+
+// Example 1:
+let nums = [3,2,2,3]
+let val = 3
+
+removeElement(nums, val)
+
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
