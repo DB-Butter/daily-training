8f29345b91376777d157818f5642e40d2f0b10ae
DB-Butter <devbabs68@gmail.com>
2022-11-28 14:27:58 -0600

    myAnswer November2022/mon28

--- November2022/mon28.js
+++ November2022/mon28.js
@@ -4,5 +4,13 @@
 
 // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
-// You must write an algorithm with O(log n) runtime complexity.
+//myAnswer:
+
+// let result = 0 
+
+// iterate through the array
+
+// compare the target value
+
+// if the target value is greater than array[i], result ++ 
 
