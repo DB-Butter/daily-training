ac376dcd58476a9b53d5ef32b8154e225db37a17
DB-Butter <devbabs68@gmail.com>
2022-11-29 13:35:11 -0600

    myAnswer November2022/tue29

--- November2022/tue29.js
+++ November2022/tue29.js
@@ -5,3 +5,9 @@
 // Given a string s consisting of words and spaces, return the length of the last word in the string.
 
 // A word is a maximal substring consisting of non-space characters only.
+
+//myAnswer:
+
+// split the string at each space
+
+// return the length of the last word in the new array
