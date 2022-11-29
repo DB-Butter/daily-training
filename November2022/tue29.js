2981a23a0e61c901d6d8d20c1250b7123d3af6c6
DB-Butter <devbabs68@gmail.com>
2022-11-29 13:33:07 -0600

    challenge November2022/tue29

--- November2022/tue29.js
+++ November2022/tue29.js
@@ -1,2 +1,7 @@
 //Tuesday November29, 2022. Challenge: Length of Last Word - easy.
 
+//Challenge: 
+
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
