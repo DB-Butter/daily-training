0e1cca76844a5e918c42232329dce38593bb1c5a
DB-Butter <devbabs68@gmail.com>
2022-12-02 13:45:52 -0600

    example3 fri02/December2022

--- December2022/fri02.js
+++ December2022/fri02.js
@@ -39,3 +39,13 @@ plusOne([4,3,2,1])
 // Explanation: The array represents the integer 4321.
 // Incrementing by one gives 4321 + 1 = 4322.
 // Thus, the result should be [4,3,2,2].
+
+// Example 3:
+
+plusOne([9])
+
+// Input: digits = [9]
+// Output: [1,0]
+// Explanation: The array represents the integer 9.
+// Incrementing by one gives 9 + 1 = 10.
+// Thus, the result should be [1,0].
