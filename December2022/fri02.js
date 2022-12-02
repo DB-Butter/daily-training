cd320b6623c154dc7d0aa8e184784f5b05cffd2b
DB-Butter <devbabs68@gmail.com>
2022-12-02 13:34:07 -0600

    example1 fri02/December2022

--- December2022/fri02.js
+++ December2022/fri02.js
@@ -29,3 +29,13 @@ function plusOne (arr) {
     }
     console.log(arr)
 }
+
+// Example 1:
+
+plusOne([1,2,3])
+
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
