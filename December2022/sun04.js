812a3f40899f8c7faee621b1cdf6c563ebdfeb32
DB-Butter <devbabs68@gmail.com>
2022-12-04 11:32:58 -0500

    myAnswer sun04/December2022

--- December2022/sun04.js
+++ December2022/sun04.js
@@ -7,4 +7,19 @@
 // 1+1=10.
 
 //Challenge: 
+
 // Given two binary strings a and b, return their sum as a binary string.
+
+//myAnswer:
+
+// declare result, sum, i=a.length, and j=b.length
+
+// while i && j >=0,
+
+//   compute sum of digit and carry
+
+//   check sum and if 1 || 3, add 1
+
+//   compute carry and decriment i && j
+
+// return result
