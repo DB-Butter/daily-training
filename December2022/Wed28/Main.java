fa130cbe4b27fe36710b54c048025adf30101b01
DB-Butter <devbabs68@gmail.com>
2022-12-28 13:00:00 -0600

    challenge

--- December2022/Wed28/Main.java
+++ December2022/Wed28/Main.java
@@ -2,6 +2,12 @@ package December2022.Wed28;
 
 /* 1. Two Sum - challenge Easy */
 
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting...");
