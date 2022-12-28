6074ec8185c2cb93de0264388104da4591f02098
DB-Butter <devbabs68@gmail.com>
2022-12-28 13:57:01 -0600

    example 1

--- December2022/Wed28/Main.java
+++ December2022/Wed28/Main.java
@@ -16,6 +16,8 @@ public class Main {
     public static void main(String[] args) {
     int[] nums = new int[]{2,7,11,15};
     int target = 9;
+    // Output [0,1]
+    // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
     System.out.println(Arrays.toString(TwoSum(nums, target)));
     }
 
