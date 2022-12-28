5bc9835ea1624aedffdfbb92bd35693247340baa
DB-Butter <devbabs68@gmail.com>
2022-12-28 13:58:25 -0600

    example 2

--- December2022/Wed28/Main.java
+++ December2022/Wed28/Main.java
@@ -14,10 +14,15 @@ You can return the answer in any order. */
 
 public class Main {
     public static void main(String[] args) {
-    int[] nums = new int[]{2,7,11,15};
-    int target = 9;
+    // int[] nums = new int[]{2,7,11,15};
+    // int target = 9;
     // Output [0,1]
     // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+    // int[] nums = new int[]{3,2,4};
+    // int target = 6;
+    // Output: [1,2]
+
     System.out.println(Arrays.toString(TwoSum(nums, target)));
     }
 
